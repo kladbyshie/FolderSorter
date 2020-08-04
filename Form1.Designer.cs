adf0af6c798ec2c755f0881edb7e17ae27d4feab
ngaragulya <ngaragulya@gmail.com>
2020-08-04 14:12:04 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace FolderSorter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.OnOffLabel = new System.Windows.Forms.Label();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "On";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.On_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(156, 55);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 29);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Off";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Off_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Sorter Status:";
+            // 
+            // OnOffLabel
+            // 
+            this.OnOffLabel.AutoSize = true;
+            this.OnOffLabel.Location = new System.Drawing.Point(156, 20);
+            this.OnOffLabel.Name = "OnOffLabel";
+            this.OnOffLabel.Size = new System.Drawing.Size(0, 20);
+            this.OnOffLabel.TabIndex = 3;
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Folder Sorter";
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 94);
+            this.Controls.Add(this.OnOffLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Sorter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label OnOffLabel;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FolderSorter
+{
+    public partial class Form1 : Form
+    {
+        public bool Status { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static List<string[]> CSVLoad(string csvpath)
+        {
+            string[] ExtensionList = File.ReadAllLines(csvpath);
+            //third: check items and move them if need be
+
+            List<string[]> NewExtensions = new List<string[]>();
+            //creates a list of lists (each list is a line (which is another list of lines))
+            foreach (string x in ExtensionList)
+            {
+                string[] splitstring = x.Split(',');
+                NewExtensions.Add(splitstring);
+            }
+            return NewExtensions;
+        }
+        
+        static Hashtable HashLoad(List<string[]> NewExtensions)
+        {
+            Hashtable ExtensionsTable = new Hashtable();
+            for (int z = 0; z < NewExtensions.Count; z++)
+            {
+                string[] LineItem = NewExtensions[z];
+                string FileType = LineItem[0];
+                List<string> ExtensionsList = new List<string>();
+
+                for (int x = 1; x < LineItem.Length; x++)
+                {
+                    ExtensionsList.Add(LineItem[x]);
+                }
+
+                ExtensionsTable.Add(ExtensionsList, FileType);
+            }
+            return ExtensionsTable;
+        }
+
+        static List<string> FolderLoad(List<string[]> NewExtensions)
+        {
+            List<string> FolderNames = new List<string>(); //list of folders to check for and create
+            foreach (var x in NewExtensions)
+            {
+                string newitem = x[0];
+                FolderNames.Add(newitem);
+            }
+            return FolderNames;
+        }
+
+        static void FolderSetup(string DirectoryPath, List<string> FolderNames)
+        {
+            DirectoryInfo DownloadDir = new DirectoryInfo(DirectoryPath);
+
+            FileInfo[] FileInfo = DownloadDir.GetFiles();
+            foreach (string x in FolderNames)
+            {
+                string path = DirectoryPath + x;
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+            }
+        }
+
+        static void Mover(bool status, Hashtable ExtensionsTable, string DirectoryPath)
+        {
+            if (status == true)
+            {
+                DirectoryInfo LocalDownloadDir = new DirectoryInfo(DirectoryPath);
+                FileInfo[] LocalFileInfo = LocalDownloadDir.GetFiles();
+
+                foreach (FileInfo file in LocalFileInfo)
+                {
+                    string foldername = FolderFinder(file.Extension, ExtensionsTable);
+                    if (foldername.Length > 0)
+                    {
+                        try
+                        {
+                            file.MoveTo(DirectoryPath + foldername + @"\" + file.Name, true);
+                        }
+                        catch (Exception e)
+                        {
+                            continue;
+                        }
+
+                    }
+                }
+            }
+        }
+
+        static string FolderFinder(string input, Hashtable ExtensionsTable)
+        {
+            foreach (List<string> x in ExtensionsTable.Keys)
+            {
+                if (x.Contains(input))
+                {
+                    return (ExtensionsTable[x].ToString());
+                }
+            }
+            return ("");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Status = true;
+            OnOffLabel.Text = "Active";
+
+            string DirectoryPath = @"C:\Users\ngara\Downloads\";
+            string csvpath = DirectoryPath + "extensions.csv";
+
+            List<string[]> Extensions = CSVLoad(csvpath);
+            Hashtable ExtensionsTable = HashLoad(Extensions);
+            List<string> FolderNames = FolderLoad(Extensions);
+            FolderSetup(DirectoryPath, FolderNames);
+
+            //While loops in Load cause the UI to not load (because the single thread is occupied with the loop). Creating a new thread to contain
+            //the loop means it works well!
+            Thread thread = new Thread(() => 
+            {
+                while (true) 
+                {
+                    Mover(Status, ExtensionsTable, DirectoryPath);
+                    Thread.Sleep(5000);
+                }
+            });
+            thread.IsBackground = true;
+            thread.Start();
+
+
+        }
+
+        private void On_Click(object sender, EventArgs e)
+        {
+            Status = true;
+            OnOffLabel.Text = "Active";
+        }
+
+        private void Off_Click(object sender, EventArgs e)
+        {
+            Status = false;
+            OnOffLabel.Text = "Inactive";
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon.Visible = true;
+            }
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon.Visible = false;
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            this.Hide();
+            notifyIcon.Visible = true;
+        }
+    }
+}
